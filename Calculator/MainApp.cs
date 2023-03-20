13da5725717687935cbc0e285ca924a75184e017
광재 이 <leekj0430@naver.com>
2023-03-20 15:29:44 +0900

    2023-03-20/15:29

--- Calculator/MainApp.cs
+++ Calculator/MainApp.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Method
+namespace Calculator
 {
-    class Calcuator
-
+    class Calculator
     {
         public static int Plus(int a, int b)
         {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class MainApp
+    {
+        public static void Main(string[] arg)
+        {
+            int result = Calculator.Plus(3, 4);
+            Console.WriteLine(result);
+
+            result = Calculator.Minus(5, 2);
+            Console.WriteLine(result);
         }
     }
 }
