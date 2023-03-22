e25e699afae126fe760867b94419768d23af1673
광재 이 <leekj0430@naver.com>
2023-03-22 15:34:48 +0900

    2023-03-22/15:34

---
+++ UsingOut/MainApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UsingOut
+{
+    class MainApp
+    {
+        static void Divide(int a, int b, out int quotitent, out int remainder)
+        {
+            quotitent = a / b;
+            remainder = a % b;
+        }
+    }
+}
